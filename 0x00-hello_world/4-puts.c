11bb851615f0d3bb06f552f54f6fda413625aa29
sherifelsaka <sherifelsaka94@yahoo.com>
2023-03-16 08:05:16 -0700

    put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A program print with put function
+ * main - put function
  *
  * Return: Always 0 (Success)
- */ 
+ */
 int main(void)
 {
-      	puts("\"Programming is like building a multilingual puzzle");  
-return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
