6ad23d980c8efd1c692288d737788f77a16d30d9
sherifelsaka <sherifelsaka94@yahoo.com>
2023-03-21 06:09:18 -0700

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('A');
+	_putchar('L');
+	_putchar('X');
+	_putchar('\n');
+	return (0);
+}
