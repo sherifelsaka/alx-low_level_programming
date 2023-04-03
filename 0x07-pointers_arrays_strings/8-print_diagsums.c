ecba0f5e9f1d79b5cd5a6e14ff32fa9a99c2ccb8
sherifelsaka <sherifelsaka94@yahoo.com>
2023-04-03 11:49:34 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum of 2 diagonals
+ * @size: width of matrix
+ * @a:pointer to start of matrix
+ * Returm: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a+p);
+	}
+	printf("%i, %i\n", l, r);
+}
