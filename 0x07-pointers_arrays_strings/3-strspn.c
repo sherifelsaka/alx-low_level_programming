c45cca1449274ff3ce3f8afd89ff895a7b5e62a3
sherifelsaka <sherifelsaka94@yahoo.com>
2023-04-03 10:47:47 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - function get the length of substring
+ * @accept: which consist byte form
+ * @s: intial segment
+ * Return: no of byte
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, x, y;
+
+	for (x = 0; s[x] != '\0' ; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+		return (z);
+}
