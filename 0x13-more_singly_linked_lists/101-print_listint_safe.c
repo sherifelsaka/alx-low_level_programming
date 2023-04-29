c770f250860bd9e9753d6e82b6d3f883801e652e
sherifelsaka <sherifelsaka94@yahoo.com>
2023-04-29 15:14:35 -0700

    updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,48 @@
 #include "lists.h"
 
 /**
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ * Owner by Sherif Elsaka
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+		{
+			size_t count = 1;
+
+			slow = slow->next;
+
+			while (slow != fast)
+			{
+				count++;
+				slow = slow->next;
+			}
+
+			return (count);
+		}
+
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (0);
+}
+
+/**
  * print_listint_safe - Prints a listint_t linked list safely
  * @head: Pointer to the head of the list
  * Return: The number of nodes in the list
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,53 @@
 #include "lists.h"
 
 /**
+ * looped_listint_count - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ * Owner by Sherif Elsaka
+ */
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *slow, *fast;
+	size_t count = 0;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head;
+	fast = head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			do {
+				count++;
+				slow = slow->next;
+				fast = fast->next->next;
+			} while (slow != fast);
+
+			count = 1;
+
+			while (slow->next != fast)
+			{
+				count++;
+				slow = slow->next;
+			}
+
+			return (count);
+		}
+	}
+
+	return (0);
+}
+
+/**
  * free_listint_safe - Frees a listint_t list safely
  * @h: Pointer to the head of the list
  * Return: The size of the list that was freed
