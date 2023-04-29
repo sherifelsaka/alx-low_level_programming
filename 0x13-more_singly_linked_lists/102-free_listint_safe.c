6b76f20cb9062dcfbba8c3079485a334744e92cf
sherifelsaka <sherifelsaka94@yahoo.com>
2023-04-29 14:14:17 -0700

    updated

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,28 +3,28 @@
 /**
  * free_listint_safe - Frees a listint_t list safely
  * @h: Pointer to the head of the list
- *
  * Return: The size of the list that was freed
+ * Owner by Sherif Elsaka
  */
 size_t free_listint_safe(listint_t **h)
 {
-    listint_t *current, *next;
-    size_t size = 0;
+	listint_t *current, *next;
+	size_t size = 0;
 
-    if (h == NULL || *h == NULL)
-        return (0);
+	if (h == NULL || *h == NULL)
+		return (0);
 
-    for (current = *h; current != NULL; current = next)
-    {
-        next = current->next;
-        free(current);
-        size++;
+	for (current = *h; current != NULL; current = next)
+	{
+		next = current->next;
+		free(current);
+		size++;
 
-        if (next >= current)
-            break;
-    }
+		if (next >= current)
+			break;
+	}
 
-    *h = NULL;
+	*h = NULL;
 
-    return (size);
+	return (size);
 }
