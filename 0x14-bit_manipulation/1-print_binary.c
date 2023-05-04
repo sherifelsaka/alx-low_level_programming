500aec42ffe9126ee4b5d3808f951664b89f28b4
sherifelsaka <sherifelsaka94@yahoo.com>
2023-05-04 10:32:58 -0700

    updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,18 +4,12 @@
  * print_binary - Prints the binary representation of a number.
  * @n: The number to print in binary.
  * Return: void
- * Owner by Sherif Elsaka
+ * Owner By Sherif Elsaka
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
-	int i;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (i = (sizeof(unsigned long int) * 8) - 1; i >= 0; i--)
-	{
-		if ((n >> i) & mask)
-			putchar('1');
-		else
-			putchar('0');
-	}
+	putchar((n & 1) + '0');
 }
